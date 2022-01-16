48589b4061d60f10fc9cd58c5f564f27eb825eb2
Andy Lian <andylke@gmail.com>
2022-01-16 11:28:57 +0800

    Add a sample custom ToStringStrategy.

---
+++ src/main/java/im/aop/loggers/sample/bar/BarToStringStrategy.java
@@ -0,0 +1,19 @@
+package im.aop.loggers.sample.bar;
+
+import org.springframework.stereotype.Component;
+
+import im.aop.loggers.messageinterpolation.ToStringStrategy;
+
+@Component
+public class BarToStringStrategy implements ToStringStrategy {
+
+  @Override
+  public boolean canSupport(Class<?> type) {
+    return type == Bar.class;
+  }
+
+  @Override
+  public String toString(Object object) {
+    return "Bar is " + ((Bar) object).getBar();
+  }
+}
